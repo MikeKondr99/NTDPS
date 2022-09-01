53f877c8b91ba02d3be9f4332cca3dbc0343329a
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-09-01 23:16:54 +0300

    система тестов

---
+++ test/tasks/test_task1.py
@@ -0,0 +1,11 @@
+import unittest
+import src.tasks.task1
+
+class TestSum(unittest.TestCase):
+
+    def test_example(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
