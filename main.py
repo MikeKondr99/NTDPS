cf9839ad15bf81876702b969b03ac4ffca03d1c6
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-09-05 15:02:25 +0300

    добавил главный запуск

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
+from typing import Callable
 from src.tasks.football import GamesData
+from src.tasks.dictionary import WordDict
 
 def task1() -> None:
     path = input("Путь к файлу данных: ")
@@ -11,10 +13,33 @@ def task1() -> None:
                 print("Строчка " + line + " не корректна")
         print(data.get_all_info())
 
+def task2() -> None:
+    path = input("Путь к файлу: ")
+
+    with open(path) as f:
+        words_len:int = int(f.readline())
+        words:list[str] = [f.readline().rstrip('\n') for i in range(0,words_len)]
+        inputs_len:int = int(f.readline())
+        inputs:list[str] = [f.readline().rstrip('\n') for i in range(0,inputs_len)]
+        d = WordDict()
+        d.add_word(*words)
+        errors:set[str] = set()
+        for line in inputs:
+            errors = errors.union(d.errors_in(line))
+        print("\n".join(errors))
+
+
+
+tasks:dict[str,Callable[[],None]] = {
+    "t1":task1,
+    "t2":task2,
+}
+            
+
 def main() -> None:
     choice = input("Введите что запустить (t# или l#): ")
-    if choice == "t1":
-        task1()
+    if(choice in tasks):
+        tasks[choice]()
     else:
         print(f"Не существует такого задания {choice}")
 
