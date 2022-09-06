53cc41aae19dbff05cbfb88a7129c31b3ac7ec50
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-09-06 18:22:49 +0300

    добавил оформление задач 3 и 4

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-
+import os
 from typing import Callable
 
 def task1() -> None:
+    print('ЗАДАЧА 1')
     from src.tasks.football import GamesData
-    path = input("Путь к файлу данных: ")
+    path = input('Путь к файлу данных: ')
     with open(path) as f:
         lines = f.readlines()
         data = GamesData()
         for line in lines:
             if not data.add_data_str(line):
-                print("Строчка " + line + " не корректна")
+                print(f'Строчка {line} не корректна')
         print(data.get_all_info())
 
 def task2() -> None:
+    print('ЗАДАЧА 2')
     from src.tasks.dictionary import WordDict
-    path = input("Путь к файлу: ")
+    path = input('Путь к файлу: ')
 
     with open(path) as f:
         words_len:int = int(f.readline())
@@ -26,28 +28,38 @@ def task2() -> None:
         errors:set[str] = set()
         for line in inputs:
             errors = errors.union(d.errors_in(line))
-        print("\n".join(errors))
+        print('\n'.join(errors))
 
 def task3() -> None:
+    print('ЗАДАЧА 3')
     import src.tasks.db1 as db
-    db.create("files/database.sqlite")
+    db.create('files/database.sqlite')
+    pass
+
+def task4() -> None:
+    print('ЗАДАЧА 4')
+    import src.tasks.db1 as db
+    db.create('files/database.sqlite',True)
     pass
 
 
 
 tasks:dict[str,Callable[[],None]] = {
-    "t1":task1,
-    "t2":task2,
-    "t3":task3,
+    't1':task1,
+    't2':task2,
+    't3':task3,
+    't4':task4,
 }
             
 
 def main() -> None:
-    choice = input("Введите что запустить (t# или l#): ")
+    if not os.path.isdir('files'):
+        os.mkdir('files')
+    choice = input('Введите что запустить (t# или l#): ')
     if(choice in tasks):
         tasks[choice]()
     else:
-        print(f"Не существует такого задания {choice}")
+        print(f'Не существует такого задания {choice}')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- src/tasks/db1.py
+++ src/tasks/db1.py
@@ -1,36 +1,43 @@
+from operator import is_
 import sqlite3
 
-def create(path:str) -> None:
+def create(path:str,is_task4:bool=False) -> None:
+    print(f'Подключаемся {path}...')
     connection = sqlite3.connect(path)
+    print('Подключено!')
     cmd = connection.cursor()
     cmd.executescript(open('src/tasks/schema.sql').read())
-    print(f'база создана по пути {path}\n')
-    good_category_query(cmd)
-    print('\n')
-    delete_client_source(cmd)
-    print('\n')
+    print('База успешно заполнена!')
+    if is_task4:
+        delete_client_source(cmd)
+    else:
+        good_category_query(cmd)
+
 
 def good_category_query(cmd:sqlite3.Cursor) -> None:
+    print('Запрашиваем все пары товара и категории...')
     from tabulate import tabulate
-    cmd.execute("""
+    cmd.execute('''
     SELECT good.name,category.name FROM good
     LEFT JOIN category_has_good
         ON good.rowid = category_has_good.good_id
     LEFT JOIN category
         ON category.rowid = category_has_good.category_id
-    """)
+    ''')
     data = cmd.fetchall()
-    print(tabulate(data,headers=["товар", "категория"]))
+    print()
+    print(tabulate(data,headers=['товар', 'категория']))
 
 # удаляет source_id,code из таблица client и таблицу source
 def delete_client_source(cmd:sqlite3.Cursor) -> None:
     from tabulate import tabulate
-    print("\nДо изменения")
-    cmd.execute("""
+    print('\nДо изменения')
+    cmd.execute('''''''''
     SELECT * FROM client
-    """)
+    ''')
     data = cmd.fetchall()
-    print(tabulate(data,headers=["код","имя","фамилия","id-источник"]))
+    print(tabulate(data,headers=['код','имя','фамилия','id-источник']))
+    print('\nУдаляем client.code и client.source_id и таблицу source...')
     cmd.executescript('''
     DROP TABLE source;
     PRAGMA foreign_keys=off;
@@ -51,14 +58,11 @@ def delete_client_source(cmd:sqlite3.Cursor) -> None:
     PRAGMA foreign_keys=on;
 
     ''')
-    print("\nПосле изменения")
-    cmd.execute("""
+    print('\nПосле изменения')
+    cmd.execute('''''''''
     SELECT * FROM client
-    """)
+    ''')
     data = cmd.fetchall()
-    print(tabulate(data,headers=["имя", "фамилия"]))
-
-
-
+    print(tabulate(data,headers=['имя', 'фамилия']))
 
 
