e93461b7c0f5a8d801065c2a413127d9ba9ca3ee
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-09-21 17:54:20 +0300

    закончил с сортировками

--- src/labs/lab3/window.py
+++ src/labs/lab3/window.py
@@ -1,9 +1,13 @@
+import time
 import sys
 from PySide2.QtWidgets import QFileDialog, QMessageBox
 from PySide2 import QtWidgets, QtGui
 
+from typing import Callable
 import src.labs.lab3.sorts as sort
 from src.labs.lab3.Ui_window import Ui_MainWindow
+
+SortFunc = Callable[[list[sort.TNum]], list[sort.TNum]]
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -14,7 +18,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.btnLoad.triggered.connect(self.load)
         self.btnBubble.triggered.connect(self.bubble)
         self.btnSelection.triggered.connect(self.selection)
-        self.btnSelection.triggered.connect(self.quick)
+        self.btnQuick.triggered.connect(self.quick)
 
     def save(self) -> None:
         path = QFileDialog().getSaveFileName()[0]
@@ -24,16 +28,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def load(self) -> None:
         path = QFileDialog().getOpenFileName()[0]
         if path:
-            self.tbOutput.setPlainText(open(path).read())
+            self.tbInput.setPlainText(open(path).read())
+
+    def sorting(self, func: SortFunc) -> None:
+        nums = self.parse_list(self.tbInput.toPlainText())
+        start = time.time()
+        nums = func(nums)
+        stop = time.time()
+        self.statusbar.showMessage(f"{stop - start} секунд")
+        self.tbOutput.setPlainText(self.list_str(nums))
 
     def bubble(self) -> None:
-        pass
+        self.sorting(sort.bubble)
 
     def selection(self) -> None:
-        pass
+        self.sorting(sort.selection)
 
     def quick(self) -> None:
-        pass
+        self.sorting(sort.quick)
+
+    def parse_list(self, text: str) -> list[float]:
+        return [float(w.strip()) for w in text.split(',')]
+
+    def list_str(self, nums: list[float]) -> str:
+        return ",".join([str(x) for x in nums])
 
 
 def run() -> None:
