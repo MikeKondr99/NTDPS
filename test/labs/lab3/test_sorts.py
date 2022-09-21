ec1186a93231a8ae1caaa6b559d52c2f2bf6defe
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-09-21 22:59:07 +0300

    добавил тестов сортировок

--- test/labs/lab3/test_sorts.py
+++ test/labs/lab3/test_sorts.py
@@ -16,7 +16,6 @@ def prep(input: list[TNum]) -> TestData:
     return (input, sorted(input))
 
 
-input1 = [3, 2, 1]
 input2 = [45, 15, 75, 37, 14, 86, 12, 53, 53]
 input3 = [0.75, 0.234, 0.124, 0.717, 0.363, 0.934]
 input4 = [84, -68, -49, -18, 13, 67, 81, 99]
@@ -31,9 +30,12 @@ input5 = [
     " Snowman",
     " To",
 ]
+input6: list[int] = []
+input7 = [1]
 
 
-PARAMS = [prep(input1), prep(input2), prep(input3), prep(input4), prep(input5)]
+PARAMS = [prep(input2), prep(input3), prep(input4),
+          prep(input5), prep(input6), prep(input7)]
 
 
 @pytest.mark.parametrize("input,expected", PARAMS)
