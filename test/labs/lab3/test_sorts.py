b4fa76579f5541c2b4b4cdb126f7866d2e3242d8
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-09-22 16:14:47 +0300

    сделал benchmark

--- test/labs/lab3/test_sorts.py
+++ test/labs/lab3/test_sorts.py
@@ -1,12 +1,13 @@
 import random
 import pytest
 from pytest_benchmark.fixture import BenchmarkFixture  # type:ignore
-from typing import Tuple, TypeVar
+from typing import Tuple, TypeVar, Callable
 import src.labs.lab3.sorts as sort
 from src.labs.lab3.sorts import TNum
 
 random.seed(0x1C2C6D66)
 
+SortingAlgorithm = Callable[[list[TNum]], list[TNum]]
 TestData = Tuple[list[TNum], list[TNum]]
 
 SIZES = [10, 1000, 5000]
@@ -53,8 +54,13 @@ def test_quick(input: list[TNum], expected: list[TNum]) -> None:
     assert sort.quick(input) == expected
 
 
-""" @pytest.mark.parametrize("size", SIZES)
-def test_bench_quick(benchmark, size: int) -> None:  # type: ignore
+SIZES = [10, 100, 500, 1000, 1500, 2000, 3000, 4000, 5000, 7500, 10000]
+ALGORITHMS = [sort.bubble, sort.selection, sort.quick]
+
+
+@pytest.mark.parametrize("size", SIZES)
+@pytest.mark.parametrize("algorithm", ALGORITHMS)
+def test_bench_sort(benchmark, size: int, algorithm: SortingAlgorithm) -> None:  # type: ignore
     data = [random.randint(-10000, 10000) for i in range(0, size)]
-    result = benchmark(sort.quick, data)
-    assert result == sorted(data) """
+    result = benchmark(algorithm, data)
+    assert result == sorted(data)
